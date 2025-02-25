d45ae2811f3c27a778ceed89be3bd9a34d3b6212
Jhon-Oliver2025 <jonatasprojetos2013@gmail.com>
2025-02-24 22:21:30 -0300

    1.02

--- wsgi.py
+++ wsgi.py
@@ -24,7 +24,6 @@ def setup_inicial():
 setup_inicial()
 
 # Expõe a aplicação para o gunicorn
-app = app.server
-
+server = app.server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    app.run_server(debug=True, host='0.0.0.0', port=10000)
